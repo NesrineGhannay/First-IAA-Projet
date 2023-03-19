beefdb5b8b5b6d2d743c0857fc2d751c2b3e2760
bryce <bryceprofessional@gmail.com>
2023-03-19 16:33:41 +0100

    mAJ  TEST

--- main.py
+++ main.py
@@ -10,4 +10,5 @@ from Caracteristiques import *
 # classifyingImages('TestCC2', 'LogisticRegression.pkl', "SIERRA")
 
 
-#MODIFICATION COUCOU  CV
+#MODIFICATION COUCOU  CVlllllllllllll
+#kldokdoazdk
