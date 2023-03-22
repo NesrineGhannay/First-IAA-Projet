912411f78f6eb39da3a82f5fa94d4d47c4149054
ghann <nesrine.ghannay@etu.univ-amu.fr>
2023-03-22 21:16:40 +0100

    fin code régression logistique

--- RegressionLogistique.py
+++ RegressionLogistique.py
@@ -5,16 +5,6 @@ from SVM import *
 """
 A partir d'un fichier de données pris en paramètre apprend grâce à la regression logistique un model
 """
-# def logisticRegression(filedata):
-#     data = getFinalData_croped_blue(filedata)
-#     # Création d'une instance du modèle LogisticRegression avec C:  l'inverse de la force de régularisation,
-#     # liblinear l'algorithme d'optimisation utilisé pour entraîner le modèle  et max_iter: le nombre maximum
-#     # d'itérations autorisées pour la convergence
-#     lr = LogisticRegression(C=1.0, solver="liblinear", max_iter=100)
-#     # apprentissage du model
-#     learnedModel = learn_model_from_data(data, lr)
-#     return learnedModel
-
 def logisticRegression(filedata):
     data = getFinalData_croped_blue(filedata)
     # Création d'une instance du modèle LogisticRegression avec C:  l'inverse de la force de régularisation,
@@ -29,16 +19,7 @@ def getData_augmented(fileData):
         image_data = load_transform_label_train_data_svm(fileData)
         return image_data
 
-# def logisticRegressionAugmented(filedata):
-#     data = getData_augmented(filedata)
-#     # Création d'une instance du modèle LogisticRegression avec C:  l'inverse de la force de régularisation,
-#     # liblinear l'algorithme d'optimisation utilisé pour entraîner le modèle  et max_iter: le nombre maximum
-#     # d'itérations autorisées pour la convergence
-#     lr = LogisticRegression(C=0.8, solver="liblinear", max_iter=100)
-#     # apprentissage du model
-#     learnedModel = learn_model_from_data(data, lr)
-#     return learnedModel
-# 0.6988859854215377
+
 
 """
 Permet d'enregistrer le model de regression logistique appris 
@@ -53,13 +34,10 @@ def saveLogisticRegression(fileData):
 '''
 Permet d'obetenir le pourcentage de réussite de notre selon le model appris et le fichier sur lequel nous avons appris le model
 '''
-# def score_algo(learnedModel, fileData):
-#     data = getFinalData_croped_blue(fileData)
-#     return estimate_model_score(learnedModel, data, 5)
-
 def score_algo(learnedModel, fileData):
-    data = getData_augmented(fileData)
-    return estimate_model_score(logisticRegression(fileData), data, 5)
+    data = getFinalData_croped_blue(fileData)
+    return estimate_model_score(learnedModel, data, 5)
 
-# print(score_algo(logisticRegression('Data'), 'Data'))
+
 # 0.7094916250367322 (image coupée et où l'on a extrait le paramètre blue)
+# Score en augmentant les données : 0.6988859854215377
